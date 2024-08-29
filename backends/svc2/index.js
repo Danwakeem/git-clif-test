44deeb85a0180a92cd7aa94bc098ad236057fd0b
Dan Jarvis <danwakeem@gmail.com>
2024-08-28 21:45:59 -0500

    fix: Comma dangle

--- backends/svc2/index.js
+++ backends/svc2/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v4.0! Your function executed successfully!'
+      message: 'Go Serverless v4.0! Your function executed successfully!',
     })
   };
 };
